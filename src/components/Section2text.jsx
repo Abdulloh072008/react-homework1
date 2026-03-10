41fdf7edd5c9d2ca07b0ebf220887360e1329e68
Abdulloh072008 <abdullohbilol54@gmail.com>
2026-03-10 08:40:51 +0500

    homework1

---
+++ src/components/Section2text.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Section2text = () => {
+  return (
+    <div className='text-white'>
+      <h1 className='text-[40px]'>Продукция завода</h1>
+      <p className='lg:w-121 text-[16px] py-8'>Ниже представлен список нашей продукции. Получить дополнительную информацию, а также оставить заявку можно по контактам коммерческой службы.</p>
+    </div>
+  )
+}
+
+export default Section2text
