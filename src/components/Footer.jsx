755f382c30111c870080798151d325ce7764e0c1
Abdulloh072008 <abdullohbilol54@gmail.com>
2026-03-11 15:45:26 +0500

    changeed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,14 +17,12 @@ const Footer = () => {
                 <img  src={group1} alt="" />
             </div>
         </div>
-        <div className='w-full'>
-            <div className='bg-white lg:w-120 flex flex-col gap-3 rounded-[10px] py-5 px-7'>
+            <div className='bg-white lg:w-120 w-full flex flex-col gap-3 rounded-[10px] py-5 px-7'>
                 <input className='border-b outline-none  border-[lightgray]  pb-3' type="text" placeholder='Имя'/>
                 <input className='border-b outline-none  border-[lightgray]  pb-3' type="text" placeholder='Почта'/>
                 <input className='border-b outline-none  border-[lightgray]  pb-3' type="text" placeholder='(971)'/>
                 <Greenbtn name="Оставить заявку" />
             </div>
-        </div>
       </div>
     </div>
   )
