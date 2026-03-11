d762617f852ed9ff7bed47c43429f05021d21e3f
Abdulloh072008 <abdullohbilol54@gmail.com>
2026-03-11 16:10:44 +0500

    changed

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,6 @@
 import React from 'react'
 
-const Nav = ({children}) => {
-  return (
-    <div className='max-w-300 w-[90%] m-auto flex items-center justify-between py-5'>
-      {children}
-    </div>
-  )
+
 import pec from '../assets/Ресурс 1 1.png'
 
 const Nav = () => {
@@ -39,12 +34,12 @@ const Nav = () => {
                     <p>RU</p>
                 </div>
             </div>
-                <div className='lg:hidden block '>
-                    <span><svg className='w-10 text-[green]' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
-                        <path fill-rule="evenodd" d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z" clip-rule="evenodd" />
-                    </svg>
-                    </span>
-                </div>
+            <div className='lg:hidden block '>
+                <span><svg className='w-10 text-[green]' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                    <path fill-rule="evenodd" d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z" clip-rule="evenodd" />
+                </svg>
+                </span>
+            </div>
         </div>
     )
 }
--- src/components/Section3.jsx
+++ src/components/Section3.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
-const Section3 = ({children}) => {
+const Section3 = ({ children }) => {
   return (
     <div className='max-w-300 w-[90%] m-auto py-15'>
-    <div className='py-15 max-w-350 w-[90%] m-auto'>
-      {children}
+
+      <div className='py-15 max-w-350 w-[90%] m-auto'>
+        {children}
+      </div>
     </div>
   )
 }
